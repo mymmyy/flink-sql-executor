779319a68d1000f76b1c967629351923ba1cb19c
maoym <maoym@g-bits.com>
2023-09-04 16:29:58 +0800

    [feature][*] 内置JSON解析函数支持多个key映射一个值模式

--- src/main/java/com/mym/flink/sqlexecutor/sqlclient/udf/JsonValueUDF.java
+++ src/main/java/com/mym/flink/sqlexecutor/sqlclient/udf/JsonValueUDF.java
@@ -9,26 +9,26 @@ public class JsonValueUDF extends ScalarFunction {
      * eval
      *
      * @param jsonString json字符串
-     * @param keyPath    key路径  如 $.name 表示取name值。只支持第一级
+     * @param keyPath    key路径  如 $.name 表示取name值。只支持第一级多个将按照COALESCE函数取值方式
      * @return {@link String}
      */
-    public String eval(String jsonString, String keyPath){
+    public String eval(String jsonString, String...keyPath){
         try {
-            if(jsonString == null || keyPath == null || "".equals(jsonString) || "".equals(keyPath)){
+            if(jsonString == null || keyPath == null || "".equals(jsonString) || keyPath.length <= 0){
                 return null;
             }
             JSONObject jsonObject = JSONObject.parseObject(jsonString);
-            Object o = jsonObject.get(keyPath.substring(2));
-            if (o == null){
-                return null;
-            } else {
-                return String.valueOf(o);
+            for (String k : keyPath) {
+                Object o = jsonObject.get(k.substring(2));
+                if (o != null){
+                    return String.valueOf(o);
+                }
             }
         }catch (Exception e){
             e.printStackTrace();
             System.out.println("parse json error! jsonString:" + jsonString);
         }
-        return "";
+        return null;
     }
 
 }
